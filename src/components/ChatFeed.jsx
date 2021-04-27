ddc2ba959eb6c8b415c71bc822098fa716af3caa
Nicholas Radtke <Raddnc@gmail.com>
2021-04-26 22:20:06 -0500

    added what will be needed, taking break

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,3 +1,14 @@
 import React from 'react'
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+import MessageForm from './MessageForm';
 
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+  
+    const chat = chats && chats[activeChat];
+  
+    
+  };
+  
   export default ChatFeed; 
