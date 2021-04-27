b27ca81bab2858eecb7aa58ba6c28443709d7238
Nicholas Radtke <Raddnc@gmail.com>
2021-04-26 22:17:01 -0500

    finished messageForm

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -25,7 +25,30 @@ const MessageForm = (props) => {
   };
 
   return (
-
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: 'none' }}
+        onChange={handleUpload.bind(this)}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
+    </form>
   );
 };
 
