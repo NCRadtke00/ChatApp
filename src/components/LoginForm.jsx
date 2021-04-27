893e854fdc3b9a17123614284610c7a6e732d0e1
Nicholas Radtke <Raddnc@gmail.com>
2021-04-26 22:08:33 -0500

    added layout

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,14 +8,22 @@ const Modal = () => {
 
 
     
-  
+ 
 
   return (
     <div className="wrapper">
       <div className="form">
         <h1 className="title">Chat Application</h1>
-        
-        
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start chatting</span>
+            </button>
+          </div>
+        </form>
+        <h1>{error}</h1>
       </div>
     </div>
 
