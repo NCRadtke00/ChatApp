9effba21dd4faab1104ad5414a59ad35158431bb
Nicholas Radtke <Raddnc@gmail.com>
2021-04-26 22:09:03 -0500

    added functionality

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,11 +4,23 @@ import axios from 'axios';
 
 
 const Modal = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
 
-
-
-    
- 
+    try {
+      await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+      window.location.reload();
+      setError('');
+    } catch (err) {
+      setError('Oops, incorrect credentials.');
+    }
+  };
 
   return (
     <div className="wrapper">
