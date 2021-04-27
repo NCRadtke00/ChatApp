be8ca307f8b38f2c024c4be7df1a8c6634405e51
Nicholas Radtke <Raddnc@gmail.com>
2021-04-26 22:22:30 -0500

    finished app just need to add styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('').play()}
+    />
+  );
+};
 
 
 export default App;
