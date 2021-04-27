973c9a3028149e3d811c65bacc591dda855cb663
Nicholas Radtke <Raddnc@gmail.com>
2021-04-26 22:16:04 -0500

    setting up messageForm

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,4 +1,19 @@
+import { useState } from 'react';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { sendMessage, isTyping } from 'react-chat-engine';
+
 const MessageForm = (props) => {
+  const [value, setValue] = useState('');
+  const { chatId, creds } = props;
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    isTyping(props, chatId);
+  };
+
+
+
+
 
   return (
 
